70d797e466f0a861964288977c131a39a7e23c5a
vlssergey09 <vls.sergey09@mail.ru>
2026-06-04 16:20:47 +0300

    Add files via upload

---
+++ item.js
@@ -0,0 +1,75 @@
+let productName =
+        document.getElementById("product-name").textContent;
+
+    dataLayer.push({
+        ecommerce: {
+            currencyCode: "RUB",
+            detail: {
+                products: [
+                    {
+                        id: "12345",
+                        name: productName,
+                        price: 1345.67,
+                        brand: "Skyrim",
+                        category: "Swords",
+                        quantity: 1
+                    }
+                ]
+            }
+        }
+    });
+
+
+document.getElementById("buyButton").onclick = function () {
+
+    let productName =
+        document.getElementById("product-name").textContent;
+
+    dataLayer.push({
+        ecommerce: {
+            currencyCode: "RUB",
+
+            purchase: {
+                actionField: {
+                    id: "TEST_ORDER_1"
+                },
+
+                products: [
+                    {
+                        id: "12345",
+                        name: productName,
+                        price: 1345.67,
+                        brand: "Skyrim",
+                        category: "Swords",
+                        quantity: 1
+                    }
+                ]
+            }
+        }
+    });
+};
+
+document.getElementById("addToCartButton").onclick = function()
+{
+ let productName =
+        document.getElementById("product-name").textContent;
+
+    dataLayer.push({
+        ecommerce: {
+            currencyCode: "RUB",
+            add: {
+                products: [
+                    {
+                        id: "12345",
+                        name: productName,
+                        price: 1345.67,
+                        brand: "Skyrim",
+                        category: "Swords",
+                        quantity: 1
+                    }
+                ]
+            }
+        }
+    });
+
+}
